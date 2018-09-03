cca70161c79ab6218d03ed721e139ddbcc0a68d2
leoshak <leoshakshared@GMAIL.COM>
2018-09-04 02:43:52 +0530

    nav_items activated for few functions that works properly and dashboard home changed

--- resources/views/admin/appointments/show.blade.php
+++ resources/views/admin/appointments/show.blade.php
@@ -6,11 +6,11 @@
     <div class="row">
         <table class="table table-striped table-hover">
             <tbody>
-            <tr>
+            {{-- <tr>
                 <th>{{ __('views.admin.users.show.table_header_0') }}</th>
                 <td><img src="https://png.icons8.com/ios/1600/user-male-circle-filled.png"  alt="Pic" height="90" width="90" class="user-profile-image"></td>
-                {{-- {{ $employee->avatar }} --}}
-            </tr>
+                {{-- {{ $employee->avatar }} 
+            </tr>--}}
 
             <tr>
                 <th>Name</th>
